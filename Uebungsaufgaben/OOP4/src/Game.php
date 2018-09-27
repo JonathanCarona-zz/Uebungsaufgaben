eb489901363c94f066970876b4a721c6e5a55b64
JonathanCarona <jonathan.carona@competec.ch>
2018-09-27 10:42:56 +0200

    OOP4

--- Uebungsaufgaben/OOP4/src/Game.php
+++ Uebungsaufgaben/OOP4/src/Game.php
@@ -17,6 +17,7 @@ class Game
      * @var LoggerInterface
      */
     private $logger;
+    private $gameOver = false;
 
     public function __construct(Configuration $configuration, LoggerInterface $logger, Dice $dice)
     {
@@ -41,34 +42,36 @@ class Game
             sleep(1);
         }
 
+        while (!$this->gameOver) {
 
-        for ($i = 0; $i < count($this->players); $i++) {
-            /** @var Player $player */
-            $player = $this->players[$i];
-            $colorDice = $this->dice->roll();
-            echo 'The dice color is ' . $colorDice . PHP_EOL;
-            /** @var array | Card $playerCards */
-            $playerCards = $player->getCards();
-            for ($ii = 0; $ii < count($playerCards); $ii++) {
-                /** @var Card $playerCard */
-                $playerCard = $playerCards[$ii];
-                $playerCard->flipEqualColorCard($playerCard, $colorDice);
-                if ($playerCard->getIsCovered()) {
-                    echo $player->getName() . '`s ' . $playerCard . ' is covered' . PHP_EOL;
-                } else {
-                    echo $player->getName() . '`s ' . $playerCard . ' is still active' . PHP_EOL;
-                }
+            for ($i = 0; $i < count($this->players); $i++) {
+                /** @var Player $player */
+                $player = $this->players[$i];
+                $colorDice = $this->dice->roll();
+                echo 'The dice color is ' . $colorDice . PHP_EOL;
                 sleep(1);
+                /** @var array | Card $playerCards */
+                $playerCards = $player->getCards();
+                for ($ii = 0; $ii < count($playerCards); $ii++) {
+                    /** @var Card $playerCard */
+                    $playerCard = $playerCards[$ii];
+                    $playerCard->flipEqualColorCard($playerCard, $colorDice);
+                    if ($playerCard->getIsCovered()) {
+                        echo $player->getName() . '`s ' . $playerCard . ' is covered' . PHP_EOL;
+                    } else {
+                        echo $player->getName() . '`s ' . $playerCard . ' is still active' . PHP_EOL;
+                    }
+                    sleep(1);
+                }
+
+                if ($player->checkIfAllCardsAreFlipped()) {
+                    echo $player->getName() . ' has won the game';
+                    $this->gameOver = true;
+                    exit();
+                }
+
+                echo PHP_EOL;
             }
-
-            if ($player->checkIfAllCardsAreFlipped()) {
-                echo $player->getName() . ' has won the game';
-                exit();
-            }
-
-            echo PHP_EOL;
-
-
         }
     }
 
@@ -85,5 +88,4 @@ class Game
         }
         echo PHP_EOL;
     }
-
 }
--- Uebungsaufgaben/OOP4/tests/GameTest.php
+++ Uebungsaufgaben/OOP4/tests/GameTest.php
@@ -11,21 +11,42 @@ class GameTest extends TestCase
 {
     /** @var Game */
     private $game;
-    /** @var Configuration */
+    /** @var Configuration | PHPUnit_Framework_MockObject_MockObject */
     private $configuration;
-    /** @var StandardOutLogger */
+    /** @var StandardOutLogger | PHPUnit_Framework_MockObject_MockObject*/
     private $logger;
-    /** @var Dice */
+    /** @var Dice | PHPUnit_Framework_MockObject_MockObject*/
     private $dice;
+    /** @var Player | PHPUnit_Framework_MockObject_MockObject */
+    private $player;
+    /** @var Color | PHPUnit_Framework_MockObject_MockObject */
+    private $color;
+
+
 
     protected function setUp()
     {
+        $this->player = $this->createMock(Player::class);
+        $arrayPlayer = array($this->player);
+        $this->color = $this->createMock(Color::class);
+        $arrayColor = array($this->color);
         $this->configuration = $this->createMock(Configuration::class);
+        $this->configuration->method('getConfNumberOfCards')->willReturn(1);
+        $this->configuration->method('getConfPlayers')->willReturn($arrayPlayer);
+        $this->configuration->method('getConfNumberOfPlayers')->willReturn(1);
+        $this->configuration->method('getConfPossibleColors')->willReturn($arrayColor);
         $this->logger = $this->createMock(StandardOutLogger::class);
         $this->dice = $this->createMock(Dice::class);
         $this->game = new Game($this->configuration, $this->logger, $this->dice);
     }
 
+
+    public function testPlayGame()
+    {
+        $card = $this->createMock(Card::class);
+        $this->game->playGame();
+
+    }
 
 
 }
